95e3ca7bc5047322d4085d6dfedee378af1a5d76
sachinM444 <127882560+SachinM44@users.noreply.github.com>
2024-08-31 21:59:49 +0530

    1.1

---
+++ App.jsx
@@ -0,0 +1,29 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import HomeScreen from "./src/screen/HomeScreen";
+import LoginScreen from "./src/screen/LoginScreen";
+import RegisterScreen from "./src/screen/RegisterScreen";
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name={"HOME"} component={HomeScreen} />
+        <Stack.Screen name={"LOGIN"} component={LoginScreen} />
+        <Stack.Screen name={"REGISTER"} component={RegisterScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
---
+++ src/screen/RegisterScreen.jsx
@@ -0,0 +1,205 @@
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useState } from "react";
+import { colors } from "../utils/colors";
+import { fonts } from "../utils/fonts";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import SimpleLineIcons from "react-native-vector-icons/SimpleLineIcons";
+import { useNavigation } from "@react-navigation/native";
+
+const RegisterScreen = () => {
+  const navigation = useNavigation();
+  const [secureEntry, setSecureEntry] = useState(true);
+
+  const handleGoBack = () => {
+    navigation.goBack();
+  };
+
+  const handleLogin = () => {
+    navigation.navigate("LOGIN");
+  };
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.backButtonWrapper} onPress={handleGoBack}>
+        <Ionicons
+          name={"arrow-back-outline"}
+          color={colors.primary}
+          size={25}
+        />
+      </TouchableOpacity>
+      <View style={styles.textContainer}>
+        <Text style={styles.headingText}>Let's get</Text>
+        <Text style={styles.headingText}>started</Text>
+      </View>
+      {/* form  */}
+      <View style={styles.formContainer}>
+        <View style={styles.inputContainer}>
+          <Ionicons name={"person-outline"} size={30} color={colors.secondary} />
+          <TextInput
+            style={styles.textInput}
+            placeholder="Enter your name"
+            placeholderTextColor={colors.secondary}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <Ionicons name={"mail-outline"} size={30} color={colors.secondary} />
+          <TextInput
+            style={styles.textInput}
+            placeholder="Enter your email"
+            placeholderTextColor={colors.secondary}
+            keyboardType="email-address"
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <SimpleLineIcons
+            name={"screen-smartphone"}
+            size={30}
+            color={colors.secondary}
+          />
+          <TextInput
+            style={styles.textInput}
+            placeholder="Enter your mobile number"
+            placeholderTextColor={colors.secondary}
+            keyboardType="phone-pad"
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <SimpleLineIcons name={"lock"} size={30} color={colors.secondary} />
+          <TextInput
+            style={styles.textInput}
+            placeholder="Enter your password"
+            placeholderTextColor={colors.secondary}
+            secureTextEntry={secureEntry}
+          />
+          <TouchableOpacity
+            onPress={() => {
+              setSecureEntry((prev) => !prev);
+            }}
+          >
+            <SimpleLineIcons name={"eye"} size={20} color={colors.secondary} />
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity style={styles.registerButtonWrapper}>
+          <Text style={styles.registerText}>Register</Text>
+        </TouchableOpacity>
+        <Text style={styles.continueText}>or continue with</Text>
+        <TouchableOpacity style={styles.googleButtonContainer}>
+          <Image
+            source={require("../assets/google.png")}
+            style={styles.googleImage}
+          />
+          <Text style={styles.googleText}>Google</Text>
+        </TouchableOpacity>
+        <View style={styles.footerContainer}>
+          <Text style={styles.accountText}>Already have an account!</Text>
+          <TouchableOpacity onPress={handleLogin}>
+            <Text style={styles.loginText}>Login</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default RegisterScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.white,
+    padding: 20,
+  },
+  backButtonWrapper: {
+    height: 40,
+    width: 40,
+    backgroundColor: colors.gray,
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textContainer: {
+    marginVertical: 20,
+  },
+  headingText: {
+    fontSize: 32,
+    color: colors.primary,
+    fontFamily: fonts.SemiBold,
+  },
+  formContainer: {
+    marginTop: 20,
+  },
+  inputContainer: {
+    borderWidth: 1,
+    borderColor: colors.secondary,
+    borderRadius: 100,
+    paddingHorizontal: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 2,
+    marginVertical: 10,
+  },
+  textInput: {
+    flex: 1,
+    paddingHorizontal: 10,
+    fontFamily: fonts.Light,
+  },
+  forgotPasswordText: {
+    textAlign: "right",
+    color: colors.secondary,
+    marginVertical: 10,
+  },
+  registerButtonWrapper: {
+    backgroundColor: colors.primary,
+    padding: 15,
+    borderRadius: 100,
+    alignItems: "center",
+    marginVertical: 10,
+  },
+  registerText: {
+    color: colors.white,
+    fontFamily: fonts.SemiBold,
+  },
+  continueText: {
+    textAlign: "center",
+    color: colors.secondary,
+    marginVertical: 10,
+  },
+  googleButtonContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+    borderColor: colors.secondary,
+    padding: 10,
+    borderRadius: 100,
+    marginVertical: 10,
+  },
+  googleImage: {
+    width: 20,
+    height: 20,
+    marginRight: 10,
+  },
+  googleText: {
+    color: colors.secondary,
+    fontFamily: fonts.SemiBold,
+  },
+  footerContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginVertical: 20,
+  },
+  accountText: {
+    color: colors.secondary,
+  },
+  loginText: {
+    color: colors.primary,
+    marginLeft: 5,
+  },
+});
